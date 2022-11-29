ac56e8becf18479e9cfd6ac402d982befba052de
gabezy <gabrielmoreira2705@gmail.com>
2022-11-29 17:44:49 -0300

    add gitignore

--- js/script.js
+++ js/script.js
@@ -94,17 +94,23 @@ function initAnimateScroll() {
 initAnimateScroll();
 
 //
-const percentages = ["100%", "120%", "150%", "175%", "200%"];
 
-let currentPerc = 0;
-function handelClickT(event) {
-  if (event.key === "+" && currentPerc < 5) {
-    ++currentPerc;
-    document.documentElement.style.fontSize = percentages[currentPerc];
-  } else if (event.key === "-" && currentPerc > -1) {
-    --currentPerc;
-    document.documentElement.style.fontSize = percentages[currentPerc];
+function initZoom() {
+  window.addEventListener("keydown", zoom);
+
+  const fontSiezes = ["100%", "120%", "150%", "175%", "200%"];
+
+  let currentPerc = 0;
+
+  function zoom(event) {
+    if (event.key === "+" && currentPerc < 5) {
+      ++currentPerc;
+      document.documentElement.style.fontSize = fontSiezes[currentPerc];
+    } else if (event.key === "-" && currentPerc > -1) {
+      --currentPerc;
+      document.documentElement.style.fontSize = fontSiezes[currentPerc];
+    }
   }
 }
 
-window.addEventListener("keydown", handelClickT);
+initZoom();
