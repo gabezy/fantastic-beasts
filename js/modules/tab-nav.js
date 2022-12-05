50dd36b352ee707572473d02b3f315b338f2d5b1
gabezy <gabrielmoreira2705@gmail.com>
2022-12-04 22:51:47 -0300

    tabNav transform to class

--- js/modules/tab-nav.js
+++ js/modules/tab-nav.js
@@ -1,23 +1,32 @@
-export default function initTabNav() {
-  const activateClass = "activate";
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activateClass = "activate";
+  }
+  activateTab(index) {
+    const currentTabContent = this.tabContent[index];
+    // take the "data-anime" atrribute's
+    const anime = currentTabContent.dataset.anime;
+    //remove activateClass for all tabContent
+    this.tabContent.forEach((content) =>
+      content.classList.remove(this.activateClass)
+    );
+    currentTabContent.classList.add(this.activateClass, anime);
+  }
 
-  const tabMenu = document.querySelectorAll("[data-tab='menu'] li");
-  const tabcontent = document.querySelectorAll("[data-tab='content'] section");
-
-  if (tabcontent.length && tabMenu.length) {
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener("click", () => {
-        activateTab(index);
-      });
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => this.activateTab(index));
     });
+  }
 
-    tabcontent[0].classList.add(activateClass, tabcontent[0].dataset.anime);
-
-    function activateTab(index) {
-      const currentTabContent = tabcontent[index];
-      const anime = currentTabContent.dataset.anime;
-      tabcontent.forEach((content) => content.classList.remove(activateClass));
-      currentTabContent.classList.add(activateClass, anime);
+  init() {
+    if (this.tabContent.length && this.tabMenu.length) {
+      // activate first tab
+      this.activateTab(0);
+      this.addTabNavEvent();
+      return this;
     }
   }
 }
