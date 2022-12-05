db5818842e6387b3ec7dbcb64dad23e951793c3a
gabezy <gabrielmoreira2705@gmail.com>
2022-12-05 20:08:34 -0300

    refacto fecth-bitcoin.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ import initDropdownMenu from "./modules/dropdown-menu.js";
 import iniMenuMobile from "./modules/menu-mobile.js";
 import initOpening from "./modules/opening.js";
 import fetchAnimals from "./modules/fetch-animals.js";
-import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import fetchBitcoin from "./modules/fetch-bitcoin.js";
 import initAnimateScroll from "./modules/animate-scroll.js";
 
 const smoothScroll = new SmoothScroll(
@@ -20,19 +20,16 @@ const tabNav = new TabNav(
   "[data-tab='menu'] li",
   "[data-tab='content'] section"
 ).init();
-
 const accordion = new Accordion("[data-anime='accordion'] dt").init();
-
 const modal = new Modal(
   "[data-modal='open']",
   "[data-modal='close']",
   "[data-modal='container']"
 ).init();
-
 const tooltip = new Tooltip("[data-tooltip]", "(min-width: 800px)").init();
 initDropdownMenu();
 iniMenuMobile();
 initOpening();
 
 fetchAnimals("./animais-api.json", ".grid-numbers");
-initFetchBitcoin();
+fetchBitcoin("https://blockchain.info/ticker", "[data-btc]", 1000);
